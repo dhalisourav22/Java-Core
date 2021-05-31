7d7f543f402314fd35f9a6317e995be0e1f0d272
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-05-31 21:18:31 +0600

    Add files via upload

---
+++ GettingTheType/test.java
@@ -0,0 +1,8 @@
+package GettingTheType;
+
+public class test {
+    public static void main(String[] args) {
+        var v = new var("name");
+        
+    }
+}
---
+++ GettingTheType/var.java
@@ -0,0 +1,12 @@
+package GettingTheType;
+
+public class var<E> {
+    E n;
+
+    public var(E x) {
+        System.out.println("X Is : "+x.getClass().getName());
+    }
+    
+    
+    
+}
