d9882f041da98eb397ed8aadcb31a693493060be
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-06-01 17:02:39 +0600

    Add files via upload

---
+++ AreaOfABox/Box.java
@@ -0,0 +1,22 @@
+package AreaOfABox;
+
+public class Box {
+
+    double height, depth, width;
+
+    Box(double height, double width, double depth) {
+        this.height = height;
+        this.width = width;
+        this.depth = depth;
+    }
+
+    void display() {
+        double vol = height * width * depth;
+        System.out.println("voloum is : " + vol);
+    }
+
+    public static void main(String[] args) {
+        Box b = new Box(10, 20, 30);
+        b.display();
+    }
+}
