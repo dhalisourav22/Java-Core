3766242cd5e15d82d04598aee52e8ffd9e5dd817
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-06-01 14:11:21 +0600

    Add files via upload

---
+++ StringBuffer/String1.java
@@ -0,0 +1,11 @@
+package StringJava;
+
+public class String1 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Sourav");
+        System.out.println(sb);
+        
+        
+    }
+    
+}
