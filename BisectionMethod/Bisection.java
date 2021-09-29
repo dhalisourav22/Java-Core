95ed8824c78c60c0e8e2a80145724ee1153d7271
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-09-29 13:02:46 +0600

    Add files via upload

---
+++ BisectionMethod/Bisection.java
@@ -0,0 +1,34 @@
+package BisectionMethod;
+public class Bisection {
+    public static void main(String[] args) {
+        double a = 0, b = 1, c = 0, trueError = 0.001, error = a-b;
+        int iterationNumber = 0;
+        while(Math.abs(error)>trueError && iterationNumber<15 && f(c)!=0){   //If any condition of those three condition will be break, then the loop must be end.
+            double cPrev = c;   //Previous Root (Relative Error).
+            c = (a+b)/2;
+            if(f(a)*f(c)<0){
+                b=c;
+            }
+            else{
+                a=c;
+            }
+            error = (cPrev-c)/cPrev;  //Relative Error.
+//            error = a-b;   //Absolute Error.
+            iterationNumber++;
+            System.out.println("Iteration Number : "+iterationNumber);
+            System.out.println("A = "+a);
+            System.out.println("B = "+b);
+            System.out.println("f(A) = "+f(a));
+            System.out.println("f(B) = "+f(b));
+            System.out.println("C/Root = "+c);
+            System.out.println("f(C) = "+f(c));
+            System.out.println("Estimated Error = "+error+"\n");
+        }
+        System.out.println("\n\n\n----------------------------------------------\n\n");
+        System.out.println("Root Is : "+c);
+        System.out.println("Error Is : "+error);
+    }
+    public static double f(double x){
+        return 4*Math.pow(x, 3) - 6*Math.pow(x, 2) + 7*x - 2.3 ;
+    }
+}
