9af595be7da559f30a7df258f72122b07a70f33e
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-10-03 15:55:33 +0600

    Update Falsi.java

--- RegularFalsiMethod/Falsi.java
+++ RegularFalsiMethod/Falsi.java
@@ -1,12 +1,14 @@
 package RegularFalsiMethod;
 public class Falsi {
-        public static void main(String[] args) {
-        double a = 0, b = 1, c = 0, trueError = 0.001, cPrev =0, error = a-b;
+    public static void main(String[] args) {
+        double a = 0, b = 1, cPresent = 0, trueError = 0.001, cPrev, error = a-b;
         int iterationNumber = 0;
+        java.text.DecimalFormat df3 = new java.text.DecimalFormat("0.000");
         
-        while(Math.abs(error)>trueError && iterationNumber<20 && f(c)!=0){   //If any condition of those three condition will be break, then the loop must be end.
-            cPrev = c;   //Previous Root (Relative Error) And This Is Also Need For Final Answer (Root).
-            c = ((a*f(b))-(b*f(a)))/(f(b)-f(a));
+        while(Math.abs(error)>trueError && iterationNumber<20 && f(cPresent)!=0){   //If any condition of those three condition will be break, then the loop must be end.
+            
+            cPrev = cPresent;   //Previous Root (Relative Error).
+            cPresent = ((a*f(b))-(b*f(a)))/(f(b)-f(a));
             
             iterationNumber++;
             System.out.println("Iteration Number : "+iterationNumber);
@@ -15,21 +17,20 @@ public class Falsi {
             System.out.println("f(A) = "+f(a));
             System.out.println("f(B) = "+f(b));
             
-            if(f(a)*f(c)<0){
-                b=c;
+            if(f(a)*f(cPresent)<0){
+                b=cPresent;
             } else{
-                a=c;
+                a=cPresent;
             }
-            error = (cPrev-c)/cPrev;  //Relative Error.
+            error = (cPresent-cPrev)/cPresent;  //Relative Error.
 //            error = a-b;  //Absolute Error
             
-            System.out.println("C/Root = "+c);
-            System.out.println("f(C) = "+f(c));
-            System.out.println("Estimated Error = "+error+"\n");
-        }
+            System.out.println("C/Root = "+cPresent);
+            System.out.println("f(C) = "+f(cPresent));
+            System.out.println("Estimated Error = "+df3.format(Math.abs(error*100))+"%\n");
+        }        
         System.out.println("\n\n\n----------------------------------------------\n\n");
-        System.out.println("Root Is : "+cPrev+"\n\n");
-
+        System.out.println("Root Is : "+df3.format(cPresent)+"\n\n");
     }
     public static double f(double x){
         return 3*x - Math.cos(x) - 1;
